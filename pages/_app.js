492a12575e2d3d7245b8f5b050524b62c0964856
Peter Durham <peterdurham@protonmail.com>
2022-10-05 20:29:19 -0400

    moved analytics script tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,7 @@
-import Script from 'next/script'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-WXES529WYF"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-WXES529WYF');
-        `}
-      </Script>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
